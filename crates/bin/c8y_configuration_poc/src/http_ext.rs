a8cbc0800203132eadd66c1a1c42c6350a12d960
Didier Wenzek <didier.wenzek@free.fr>
2022-11-30 18:50:49 +0100

    Add a multi-clients HTTP actor
    Signed-off-by: Didier Wenzek <didier.wenzek@free.fr>

--- crates/bin/c8y_configuration_poc/src/http_ext.rs
+++ crates/bin/c8y_configuration_poc/src/http_ext.rs
@@ -1,6 +1,8 @@
 use async_trait::async_trait;
+use std::ptr::addr_of;
 use tedge_actors::{
-    new_mailbox, Actor, ChannelError, Mailbox, Recipient, RuntimeError, RuntimeHandle,
+    new_mailbox, Actor, Address, ChannelError, Mailbox, Message, Recipient, RuntimeError,
+    RuntimeHandle, Sender,
 };
 
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
@@ -67,5 +69,68 @@ impl Actor for PrivateHttpActor {
         }
 
         Ok(())
+    }
+}
+
+/// Create a new HTTP connection managed behind the scene by an actor
+///
+/// This connection can be shared by several clients,
+pub struct HttpActorInstance {
+    config: HttpConfig,
+    mailbox: Mailbox<(usize, HttpRequest)>,
+    address: Address<(usize, HttpRequest)>,
+    clients: Vec<Recipient<HttpResponse>>,
+}
+
+impl HttpActorInstance {
+    pub fn new(config: HttpConfig) -> Self {
+        let (mailbox, address) = new_mailbox(10);
+
+        HttpActorInstance {
+            config,
+            mailbox,
+            address,
+            clients: vec![],
+        }
+    }
+
+    pub fn add_client(&mut self, client: Recipient<HttpResponse>) -> Recipient<HttpRequest> {
+        let client_idx = self.clients.len();
+        self.clients.push(client);
+
+        KeyedRecipient::new(client_idx, self.address.clone())
+    }
+
+    pub async fn spawn(self, runtime: &mut RuntimeHandle) -> Result<(), RuntimeError> {
+        todo!();
+    }
+}
+
+pub struct KeyedRecipient<M: Message> {
+    idx: usize,
+    address: Address<(usize, M)>,
+}
+
+impl<M: Message> KeyedRecipient<M> {
+    pub fn new(idx: usize, address: Address<(usize, M)>) -> Recipient<M> {
+        Box::new(KeyedRecipient { idx, address })
+    }
+
+    pub fn clone(&self) -> Recipient<M> {
+        Box::new(KeyedRecipient {
+            idx: self.idx,
+            address: self.address.clone(),
+        })
+    }
+}
+
+#[async_trait]
+impl<M: Message> Sender<M> for KeyedRecipient<M> {
+    async fn send(&mut self, message: M) -> Result<(), ChannelError> {
+        self.address.send((self.idx, message)).await
+    }
+
+    fn recipient_clone(&self) -> Recipient<M> {
+        self.clone()
     }
 }
